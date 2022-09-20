5137e0533becae43d52ea292fbf2c8107ceb929e
George Garman <georgecolingarman.uk@gmail.com>
2022-09-20 17:36:10 +0100

    bug fixes

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,76 +1,93 @@
 const Profile = require("../models/profileModel")
 
-const registerProfile = async(req, res) => {
-  try{
-    const duplicate = await Profile.findOne({name: fullNameToName(req.body.name)}) // *
+const registerProfile = async (req, res) => {
+  try {
+    const duplicate = await Profile.findOne({
+      name: fullNameToName(req.body.name),
+    }) // *
+    console.log("duplicate :>> ", duplicate)
     console.log(duplicate)
-    if (duplicate) return res.status(400).json({status:"fail", msg: "Profile already exists"})
+    if (duplicate)
+      return res
+        .status(400)
+        .json({ status: "fail", msg: "Profile already exists" })
+    const { birthday, role } = req.body
     const profile = new Profile({
-      birthday: birthday,
-      role: role,
-      mode: true
+      birthday,
+      role,
+      mode: true,
     })
     profile.name = name
     await profile.save()
-    res.status(201).json({status:"success", msg: "Successfully registered new profile"})
-
+    res
+      .status(201)
+      .json({ status: "success", msg: "Successfully registered new profile" })
   } catch (e) {
-    console.log("Error from route: "+e)
+    console.log("Error from route: " + e)
     res.status(500).json({
-        status: "fail",
-        msg:"Internal Server Error"
+      status: "fail",
+      msg: "Internal Server Error",
     })
   }
 }
 
-const getProfile = async(req, res) => {
-  try{
-    const profile = await Profile.findOne({_id: req.body._id})
-    if (!profile) return res.status(404).json({status: "fail", msg: "Profile not found"})
+const getProfile = async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ _id: req.body._id })
+    if (!profile)
+      return res.status(404).json({ status: "fail", msg: "Profile not found" })
     return res.status(200).json(profile)
-    
   } catch (e) {
-    console.log("Error from route: "+e)
+    console.log("Error from route: " + e)
     res.status(500).json({
-        status: "fail",
-        msg:"Internal Server Error"
+      status: "fail",
+      msg: "Internal Server Error",
     })
   }
 }
 
-const updateProfile = async(req, res) => {
-  try{
-    const profile = await Profile.findByIdAndUpdate({_id: req.body._id}, req.body)
-    if (!profile) return res.status(404).json({status: "fail", msg: "Profile not found"})
-    res.status(200).json({status: "success", msg: "Update user successfully"})
-
+const updateProfile = async (req, res) => {
+  try {
+    const profile = await Profile.findByIdAndUpdate(
+      { _id: req.body._id },
+      req.body
+    )
+    if (!profile)
+      return res.status(404).json({ status: "fail", msg: "Profile not found" })
+    res.status(200).json({ status: "success", msg: "Update user successfully" })
   } catch (e) {
-    console.log("Error from route: "+e)
+    console.log("Error from route: " + e)
     res.status(500).json({
-        status: "fail",
-        msg:"Internal Server Error"
+      status: "fail",
+      msg: "Internal Server Error",
     })
   }
 }
 
-const deleteProfile = async(req, res) => {
-  try{
-    const profile = await Profile.findByIdAndUpdate({_id: req.body._id}, {mode: false})
-    if (!profile) return res.status(404).json({status: "fail", msg: "Profile not found"})
-    res.status(200).json({status: "success", msg: "Deleted profile successfully"})
+const deleteProfile = async (req, res) => {
+  try {
+    const profile = await Profile.findByIdAndUpdate(
+      { _id: req.body._id },
+      { mode: false }
+    )
+    if (!profile)
+      return res.status(404).json({ status: "fail", msg: "Profile not found" })
+    res
+      .status(200)
+      .json({ status: "success", msg: "Deleted profile successfully" })
   } catch (e) {
-    console.log("Error from route: "+e)
+    console.log("Error from route: " + e)
     res.status(500).json({
-        status: "fail",
-        msg:"Internal Server Error"
+      status: "fail",
+      msg: "Internal Server Error",
     })
   }
 }
 
 function fullNameToName(fullName) {
-  const name = {first: "", last: ""}
-  name.first = fullName.substr(0, fullName.indexOf(' '))
-  name.last = fullName.substr(fullName.indexOf(' ') + 1)
+  const name = { first: "", last: "" }
+  name.first = fullName.substr(0, fullName.indexOf(" "))
+  name.last = fullName.substr(fullName.indexOf(" ") + 1)
   return name
 }
 
